4f5c088def9a3f2e70d80b2166858738eaff98c8
Eric Martínez <62176881+emartg@users.noreply.github.com>
2023-02-14 19:50:18 +0100

    Cambio estructura carpetas recursos
    Cambio estructura carpetas recursos

--- src/main/java/com/urjcdad/efightclub/EFightClubApplication.java
+++ src/main/java/com/urjcdad/efightclub/EFightClubApplication.java
@@ -2,8 +2,9 @@ package com.urjcdad.efightclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EFightClubApplication {
 
 	public static void main(String[] args) {
