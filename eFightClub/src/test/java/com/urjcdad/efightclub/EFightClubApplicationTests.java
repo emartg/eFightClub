4acda1f0f9ed7f8f05e14c4444ffde4e4760ce33
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2023-06-28 21:12:39 +0200

    Create EFightClubApplicationTests.java

---
+++ eFightClub/src/test/java/com/urjcdad/efightclub/EFightClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urjcdad.efightclub.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFightClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
